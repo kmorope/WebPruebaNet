7d5f8aa7a9f9c95c12f88f229c2e4f3931ee6631
Camilo Romero <kmorope@outlook.com>
2016-10-20 17:17:14 -0500

    NEW - Added angular - Added jquery confirm dialogs - Added initial scripts. IMPROVEMENTS - Removed default views and methods from initial created proyect

--- src/WebPrueba/wwwroot/js/site.js
+++ src/WebPrueba/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿// Write your Javascript code.
+﻿function createDialog(title, content) {
+    $.alert({
+        title: title,
+        content: content,
+        confirm: function () {
+            $.alert('Confirmed!');
+        }
+    });
+}
+
+$(document).on('click','.js-btn-add-person',function() {
+    createDialog('Prueba', '<div class=\"btn btn-default\">Hola Mundo</div>');
+});
